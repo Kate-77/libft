b0eaae5024e4a6841990f45f8cbdb8dce9be7bf2
Kaoutar Moutaouakil <kmoutaou@e1r12p9.1337.ma>
2021-11-08 20:05:47 +0100

    ft_strtrim.c needs to be fixed

--- srcs/ft_strlcpy.c
+++ srcs/ft_strlcpy.c
@@ -6,18 +6,28 @@
 /*   By: kmoutaou <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/07 10:48:32 by kmoutaou          #+#    #+#             */
-/*   Updated: 2021/11/07 12:03:42 by kmoutaou         ###   ########.fr       */
+/*   Updated: 2021/11/08 20:04:07 by kmoutaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 #include "../libft.h"
-/*
 size_t	ft_strlcpy(char *dest, const char *src, size_t n)
 {
-	unsigned int	i;
+	unsigned int	count;
+	unsigned int 	i;
 
 	i = 0;
-	while(src[i])
-		i++;
-	
-	return (i);
-}*/
+	count = 0;
+	while(src[count])
+		count++;
+	if (n != 0)
+	{
+		while (src[i] && i < n - 1)
+		{
+			dest[i] = src[i];
+			i++;
+		}
+		dest[i] = '\0';
+	}
+	return (count);
+}
